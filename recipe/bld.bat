a2fa386870f6f72b0a26a1b6143fff9def900f71
Benjamin Bertrand <beenje@gmail.com>
2022-05-13 14:44:38 +0200

    Add windows support
    * Change source to GitLab repo because the current sdist doesn't include
      the cmake files required to build on windows (will be fixed in next version)
    * Original cmake still need some patches to build with conda
    * pytest only run on Linux. No TangoTest 3.2 available for Windows right now
      and running tests on Windows is very slow as each test has to be run separately.

---
+++ recipe/bld.bat
@@ -0,0 +1,19 @@
+setlocal EnableDelayedExpansion
+
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_CXX_FLAGS="-DLOG4TANGO_HAS_DLL -DTANGO_HAS_DLL" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DTANGO_PKG_INCLUDE_DIRS:PATH="%LIBRARY_INC%\tango" ^
+      -DTANGO_ROOT:PATH="%LIBRARY_PREFIX%" ^
+      -DPYTHON_VER="%CONDA_PY%" ^
+      -DPYTHON_ROOT:PATH="%PREFIX%" ^
+      ..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit
